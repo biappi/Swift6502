0cb74970b80dd17dd008da0631e1fce8c1c527d4
Antonio Malara <antonio.malara@booking.com>
2015-12-26 18:16:46 +0100

    stubbing out a runner for console64

--- Console64/main.swift
+++ Console64/main.swift
@@ -43,144 +43,26 @@ class C64MemoryLayout : Memory {
         ram[Int(address)] = to
     }
 }
-exit(0)
 
-let c64boottrace = [
-    CpuState(A: 0x00, X: 0x00, Y: 0x00, SP: 0xff, PC: 0xfce2, SR: CpuState.StatusRegister(rawValue: 0x30)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x00, SP: 0xff, PC: 0xfce4, SR: CpuState.StatusRegister(rawValue: 0xb0)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x00, SP: 0xff, PC: 0xfce5, SR: CpuState.StatusRegister(rawValue: 0xb4)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x00, SP: 0xff, PC: 0xfce6, SR: CpuState.StatusRegister(rawValue: 0xb4)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x00, SP: 0xff, PC: 0xfce7, SR: CpuState.StatusRegister(rawValue: 0xb4)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfd02, SR: CpuState.StatusRegister(rawValue: 0xb4)),
-    CpuState(A: 0x00, X: 0x05, Y: 0x00, SP: 0xfd, PC: 0xfd04, SR: CpuState.StatusRegister(rawValue: 0x34)),
-    CpuState(A: 0x30, X: 0x05, Y: 0x00, SP: 0xfd, PC: 0xfd07, SR: CpuState.StatusRegister(rawValue: 0x34)),
-    CpuState(A: 0x30, X: 0x05, Y: 0x00, SP: 0xfd, PC: 0xfd0a, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x30, X: 0x05, Y: 0x00, SP: 0xfd, PC: 0xfd0f, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x30, X: 0x05, Y: 0x00, SP: 0xff, PC: 0xfcea, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x30, X: 0x05, Y: 0x00, SP: 0xff, PC: 0xfcef, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x30, X: 0x05, Y: 0x00, SP: 0xff, PC: 0xfcf2, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x30, X: 0x05, Y: 0x00, SP: 0xfd, PC: 0xfda3, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x7f, X: 0x05, Y: 0x00, SP: 0xfd, PC: 0xfda5, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x7f, X: 0x05, Y: 0x00, SP: 0xfd, PC: 0xfda8, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x7f, X: 0x05, Y: 0x00, SP: 0xfd, PC: 0xfdab, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x7f, X: 0x05, Y: 0x00, SP: 0xfd, PC: 0xfdae, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x08, X: 0x05, Y: 0x00, SP: 0xfd, PC: 0xfdb0, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x08, X: 0x05, Y: 0x00, SP: 0xfd, PC: 0xfdb3, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x08, X: 0x05, Y: 0x00, SP: 0xfd, PC: 0xfdb6, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x08, X: 0x05, Y: 0x00, SP: 0xfd, PC: 0xfdb9, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x08, X: 0x05, Y: 0x00, SP: 0xfd, PC: 0xfdbc, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x08, X: 0x00, Y: 0x00, SP: 0xfd, PC: 0xfdbe, SR: CpuState.StatusRegister(rawValue: 0x37)),
-    CpuState(A: 0x08, X: 0x00, Y: 0x00, SP: 0xfd, PC: 0xfdc1, SR: CpuState.StatusRegister(rawValue: 0x37)),
-    CpuState(A: 0x08, X: 0x00, Y: 0x00, SP: 0xfd, PC: 0xfdc4, SR: CpuState.StatusRegister(rawValue: 0x37)),
-    CpuState(A: 0x08, X: 0x00, Y: 0x00, SP: 0xfd, PC: 0xfdc7, SR: CpuState.StatusRegister(rawValue: 0x37)),
-    CpuState(A: 0x08, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfdc8, SR: CpuState.StatusRegister(rawValue: 0xb5)),
-    CpuState(A: 0x08, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfdcb, SR: CpuState.StatusRegister(rawValue: 0xb5)),
-    CpuState(A: 0x07, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfdcd, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x07, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfdd0, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x3f, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfdd2, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x3f, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfdd5, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0xe7, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfdd7, SR: CpuState.StatusRegister(rawValue: 0xb5)),
-    CpuState(A: 0xe7, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfdd9, SR: CpuState.StatusRegister(rawValue: 0xb5)),
-    CpuState(A: 0x2f, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfddb, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x2f, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfddd, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfde0, SR: CpuState.StatusRegister(rawValue: 0x37)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfdec, SR: CpuState.StatusRegister(rawValue: 0x37)),
-    CpuState(A: 0x95, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfdee, SR: CpuState.StatusRegister(rawValue: 0xb5)),
-    CpuState(A: 0x95, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfdf1, SR: CpuState.StatusRegister(rawValue: 0xb5)),
-    CpuState(A: 0x42, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfdf3, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x42, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfdf6, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x42, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xff6e, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x81, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xff70, SR: CpuState.StatusRegister(rawValue: 0xb5)),
-    CpuState(A: 0x81, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xff73, SR: CpuState.StatusRegister(rawValue: 0xb5)),
-    CpuState(A: 0x08, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xff76, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xff78, SR: CpuState.StatusRegister(rawValue: 0x37)),
-    CpuState(A: 0x11, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xff7a, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x11, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xff7d, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x11, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xee8e, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x07, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xee91, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x17, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xee93, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x17, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xee96, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x17, X: 0xff, Y: 0x00, SP: 0xff, PC: 0xfcf5, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x17, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfd50, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfd52, SR: CpuState.StatusRegister(rawValue: 0x37)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfd53, SR: CpuState.StatusRegister(rawValue: 0x37)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfd56, SR: CpuState.StatusRegister(rawValue: 0x37)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfd59, SR: CpuState.StatusRegister(rawValue: 0x37)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x00, SP: 0xfd, PC: 0xfd5c, SR: CpuState.StatusRegister(rawValue: 0x37)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x01, SP: 0xfd, PC: 0xfd5d, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x01, SP: 0xfd, PC: 0xfd53, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x01, SP: 0xfd, PC: 0xfd56, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x01, SP: 0xfd, PC: 0xfd59, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x01, SP: 0xfd, PC: 0xfd5c, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x02, SP: 0xfd, PC: 0xfd5d, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x02, SP: 0xfd, PC: 0xfd53, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x02, SP: 0xfd, PC: 0xfd56, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x02, SP: 0xfd, PC: 0xfd59, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x02, SP: 0xfd, PC: 0xfd5c, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x03, SP: 0xfd, PC: 0xfd5d, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x03, SP: 0xfd, PC: 0xfd53, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x03, SP: 0xfd, PC: 0xfd56, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x03, SP: 0xfd, PC: 0xfd59, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x03, SP: 0xfd, PC: 0xfd5c, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x04, SP: 0xfd, PC: 0xfd5d, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x04, SP: 0xfd, PC: 0xfd53, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x04, SP: 0xfd, PC: 0xfd56, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x04, SP: 0xfd, PC: 0xfd59, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x04, SP: 0xfd, PC: 0xfd5c, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x05, SP: 0xfd, PC: 0xfd5d, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x05, SP: 0xfd, PC: 0xfd53, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x05, SP: 0xfd, PC: 0xfd56, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x05, SP: 0xfd, PC: 0xfd59, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x05, SP: 0xfd, PC: 0xfd5c, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x06, SP: 0xfd, PC: 0xfd5d, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x06, SP: 0xfd, PC: 0xfd53, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x06, SP: 0xfd, PC: 0xfd56, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x06, SP: 0xfd, PC: 0xfd59, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x06, SP: 0xfd, PC: 0xfd5c, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x07, SP: 0xfd, PC: 0xfd5d, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x07, SP: 0xfd, PC: 0xfd53, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x07, SP: 0xfd, PC: 0xfd56, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x07, SP: 0xfd, PC: 0xfd59, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x07, SP: 0xfd, PC: 0xfd5c, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x08, SP: 0xfd, PC: 0xfd5d, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x08, SP: 0xfd, PC: 0xfd53, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x08, SP: 0xfd, PC: 0xfd56, SR: CpuState.StatusRegister(rawValue: 0x35)),
-    CpuState(A: 0x00, X: 0xff, Y: 0x08, SP: 0xfd, PC: 0xfd59, SR: CpuState.StatusRegister(rawValue: 0x35)),
-]
+var mem = C64MemoryLayout(kernal: kernal, basic: basic)
+var cpu = CpuState()
 
-let c64memory = C64MemoryLayout(
-    kernal: kernal,
-    basic:  basic
-)
+cpu.PC = mem.wordAt(0xfffc)
 
-var cpuState = CpuState()
-
-cpuState.PC = c64memory.wordAt(0xfffc)
-
-for (i, correctState) in c64boottrace.enumerate() {
-    let correct = cpuState == correctState
+for i in 0..<1000000 {
+    let s = String(format: "<PC: %04x>", arguments: [cpu.PC])
+    print(s)
     
-    if correct {
-        print ("√\n")
-    }
-    else {
-        print("Expected:")
-        print(correctState.description())
-        print("Got:")
-        print(cpuState.description())
-        print("")
-
-        print ("X")
-        break
+    if cpu.PC == 0xffd2 {
+        // CHROUT
+        fputc(Int32(cpu.A), stdout)
+        
+        var sr = cpu.SR
+        sr.remove(.C)
+        
+        cpu = cpu.change(SR: sr)
+        cpu = RTS(OpcodeValue.Implied, c: cpu, m: mem)
     }
     
-    let pc       = cpuState.PC
-    let code     = Int(c64memory.byteAt(pc))
-    let opcode   = Opcodes[code]
-    let pcString = String(format: "%2d [%04x]", i, pc)
-    
-    print(String(format:"\(pcString) Code %02x, \(opcode.0) \(opcode.1.name)", code))
-
-    cpuState = CpuStep(cpuState, memory: c64memory)
+    cpu = CpuStep(cpu, memory: mem)
 }
-
